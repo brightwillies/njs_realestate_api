55396831ab47cb03d1fdb0ebd498b2f9aba60648
brightwillies <bwboakye2@gmail.com>
2024-09-28 08:11:41 -0300

    update

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ import  cors from "cors";
 import cookieParser  from  "cookie-parser";
 app.use(express.json());
 app.use(cookieParser()); 
-// app.use(cors({origin : process.env.CLIENT_URL, credentials: true}))
-app.use(cors({origin : 'https://njs-realestate-swart.vercel.app', credentials: true}))
+ app.use(cors({origin : process.env.CLIENT_URL, credentials: true}))
+//app.use(cors({origin : 'https://njs-realestate-swart.vercel.app', credentials: true}))
 // Authentication Route 
 
 import authRoute from "./routes/auth.route.js"
